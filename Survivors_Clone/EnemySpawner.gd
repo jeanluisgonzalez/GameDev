6fdf33e8c810942975ce52d04afb9899699951a8
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2024-01-03 14:55:34 -0400

    update at work

--- Survivors_Clone/EnemySpawner.gd
+++ Survivors_Clone/EnemySpawner.gd
@@ -1,3 +1,31 @@
 extends Node2D
 
 @export var spawns: Array[Spawn_info] = []
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var time = 0
+
+func _on_timer_timeout():
+	time += 1
+	var enemy_spawns = spawns
+	for i in enemy_spawns:
+		if time >= i.time_start and time <= i.time_end:
+			if i.spawn_delay_counter < i.enemy_spawn_delay:
+				i.spawn_delay_counter += 1
+			else:
+				i.spawn_delay_counter = 0
+				var new_enemy = load(str(i.enemy.resource_path))
+				var counter = 0
+				while counter < i.enemy_num:
+					var enemy_spawn = new_enemy.instantiate()
+					enemy_spawn.global_position = get_random_position()
+					add_child(enemy_spawn)
+					counter += 1
+
+func get_random_position():
+	var vpr = get_viewport_rect().size * randf_range(1.1,1.4)
+	var top_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y - vpr.y/2)
+	var top_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y - vpr.y/2)
+	var bottom_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y + vpr.y/2)
+	var bottom_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y + vpr.y/2)
