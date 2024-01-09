77bb14e5dcc2222340ad31314a8b5090c417e2df
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2024-01-09 11:14:30 -0400

    update at work

--- Survivors_Clone/EnemySpawner.gd
+++ Survivors_Clone/EnemySpawner.gd
@@ -15,7 +15,7 @@ func _on_timer_timeout():
 				i.spawn_delay_counter += 1
 			else:
 				i.spawn_delay_counter = 0
-				var new_enemy = load(str(i.enemy.resource_path))
+				var new_enemy = i.enemy
 				var counter = 0
 				while counter < i.enemy_num:
 					var enemy_spawn = new_enemy.instantiate()
@@ -27,5 +27,26 @@ func get_random_position():
 	var vpr = get_viewport_rect().size * randf_range(1.1,1.4)
 	var top_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y - vpr.y/2)
 	var top_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y - vpr.y/2)
-	var bottom_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y + vpr.y/2)
-	var bottom_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y + vpr.y/2)
+	var botton_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y + vpr.y/2)
+	var botton_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y + vpr.y/2)
+	var pos_side = ["up","down","right","left"].pick_random()
+	var spawn_pos1 = Vector2.ZERO
+	var spawn_pos2 = Vector2.ZERO
+	
+	match pos_side:
+		"up":
+			spawn_pos1 = top_left
+			spawn_pos2 = top_right
+		"down":
+			spawn_pos1 = botton_left
+			spawn_pos2 = botton_right
+		"right":
+			spawn_pos1 = top_right
+			spawn_pos2 = botton_right
+		"left":
+			spawn_pos1 = top_left
+			spawn_pos2 = botton_left
+	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
+	var y_spawn = randf_range(spawn_pos1.y, spawn_pos2.y)
+	
+	return Vector2(x_spawn, y_spawn)
