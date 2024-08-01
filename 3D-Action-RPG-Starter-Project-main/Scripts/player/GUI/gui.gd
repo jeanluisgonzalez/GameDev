c5f2aebae6ab4a9da1f2c96e589ac55e811755d1
jeanluisgonzalez <jeanluiss26@gmail.com>
2024-08-01 17:38:41 -0400

    up

--- 3D-Action-RPG-Starter-Project-main/Scripts/player/GUI/gui.gd
+++ 3D-Action-RPG-Starter-Project-main/Scripts/player/GUI/gui.gd
@@ -12,3 +12,17 @@ func _physics_process(delta):
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			false:
 				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _on_inventory_button_pressed():
+	get_node("container/VBoxContainer/inventory_button").disabled = true
+	get_node("container/VBoxContainer/profile_button").disabled = false
+	get_node("container/inventory").show()
+	get_node("container/profile").hide()
+
+
+func _on_profile_button_pressed():
+	get_node("container/VBoxContainer/inventory_button").disabled = false
+	get_node("container/VBoxContainer/profile_button").disabled = true
+	get_node("container/inventory").hide()
+	get_node("container/profile").show()
