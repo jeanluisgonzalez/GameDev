24929ecafeb6da9eeef09394e8c137d8131a12d9
jeanluisgonzalez <jeanluiss26@gmail.com>
2024-08-04 23:55:29 -0400

    update at home

--- 3D-Action-RPG-Starter-Project-main/Scripts/player/GUI/Inventory/inventory_slot.gd
+++ 3D-Action-RPG-Starter-Project-main/Scripts/player/GUI/Inventory/inventory_slot.gd
@@ -1,11 +1,21 @@
-extends Node
+class_name InventorySlot
+extends PanelContainer
 
+@export var type: ItemData.Type
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _init(t: ItemData.Type, cms:Vector2) -> void:
+	type = t
+	custom_minimum_size = cms
 
+func _can_drop_data(at_position: Vector2, data:Variant)-> bool:
+	return false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	pass
+
+func _physics_process(delta:float) ->void:
+	#check for equipment and adjust stats
+	pass
+
+func _gui_input(event: InputEvent) -> void:
 	pass
