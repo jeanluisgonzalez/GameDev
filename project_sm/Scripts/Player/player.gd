20e01e09837d63cb80ee170ecac001b74e2d5c8a
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2024-09-23 09:12:40 -0400

    update at work

--- project_sm/Scripts/Player/player.gd
+++ project_sm/Scripts/Player/player.gd
@@ -3,12 +3,13 @@ extends CharacterBody3D
 @onready var animation_player = $visuals/character/AnimationPlayer
 @onready var visuals = $visuals
 
-
-
-const SPEED = 3.0
+var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 var walking_speed = 3.0
+var running_speed = 5.0
 
+var running = false
+var is_locked = false
 @export var sens_horizontal = 0.2
 @export var sens_vertical = 0.2
 
@@ -21,6 +22,20 @@ func _input(event):
 		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
 func _physics_process(delta):
+	if !animation_player.is_playing():
+		is_locked = false
+		
+	if Input.is_action_just_pressed("roll"):
+		if animation_player.current_animation != "Roll":
+			animation_player.play("Roll")
+			is_locked= true
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed
+		running = false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -34,15 +49,24 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if animation_player.current_animation != "Walking":
-			animation_player.play("Walking")
-		visuals.look_at(position + direction)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if !is_locked:
+			if running:
+				if animation_player.current_animation != "Running":
+					animation_player.play("Running")
+			else:
+				if animation_player.current_animation != "Walking":
+					animation_player.play("Walking")
+					
+			
+			visuals.look_at(position + direction)
+			
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 	else:
-		if animation_player.current_animation != "Idle":
-			animation_player.play("Idle")
+		if !is_locked:
+			if animation_player.current_animation != "Idle":
+				animation_player.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	if !is_locked:
+		move_and_slide()
